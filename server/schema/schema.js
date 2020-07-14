932aa5d4ee95b1acf4518631c15ac7fefcc3b0c7
BHamp <brianscotthampton@gmail.com>
2020-07-14 15:57:11 -0400

    added resolve function to query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _  = require('lodash')
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
 
 const BookType = new GraphQLObjectType({
     name:"Book",
@@ -13,12 +21,13 @@ const BookType = new GraphQLObjectType({
 
 const RootQuery = new GraphQLObjectType({
     name:'RootQueryType',
-    field: {
+    fields: {
         book: {
             type: BookType,
             args:{id: {type:GraphQLString}},
             resolve(parent, args){
                 //code to get data from db/othersource
+                return _.find(books, { id: args.id })
             }
         }
     }
