b3cfe8a6218530e13f480efde0444fed30638c25
BHamp <brianscotthampton@gmail.com>
2020-07-14 15:13:13 -0400

    Added basic schema

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-var { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema.js')
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
-
+    schema
 }));
 
 app.listen(4000, () => {console.log('listening on port 4000')})
