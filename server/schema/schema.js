1be44c6d5e271ca026792ae2a7fe918b4c60e162
BHamp <brianscotthampton@gmail.com>
2020-07-17 18:42:35 -0400

    created add author mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -65,6 +65,27 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: { type: GraphQLString },
+                age: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                }) //mongoose model
+                return author.save();
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 })
